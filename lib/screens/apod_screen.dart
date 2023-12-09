24450fbc13958eadc3824570437c4878a5ea9032
AriSevcik <99729800+AriSevcik@users.noreply.github.com>
2023-12-09 14:12:39 +0100

    Screens organization

---
+++ lib/screens/apod_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_project/models/apod.dart';
+
+class APODScreen extends StatelessWidget {
+  const APODScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screens/earth_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_project/models/epic.dart';
+
+class EarthViewScreen extends StatelessWidget {
+  const EarthViewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
