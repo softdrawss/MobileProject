f404e866dd13265c7d1b12390bd9889594aeeabd
Júlia Serra Trujillo <juliaserratrujillo@gmail.com>
2024-01-05 01:42:02 +0100

    Changed text in ss body screen

--- lib/screens/ss_body_screen.dart
+++ lib/screens/ss_body_screen.dart
@@ -3,8 +3,8 @@ import 'package:mobile_project/models/ss_body.dart';
 
 class SSBodyScreen extends StatelessWidget {
   const SSBodyScreen({super.key, required this.id});
-  
-final String id;
+
+  final String id;
 
   @override
   Widget build(BuildContext context) {
@@ -40,22 +40,64 @@ final String id;
                 ),
                 Text(picture.name),
                 Text(picture.bodyType),
-                Text("Gravity: ${picture.gravity} m/s^2"),
-                Text("Semimajor Axis: ${picture.semimajorAxis} km"),
+                Text(
+                  picture.gravity == 0
+                      ? "Gravity: Unknown"
+                      : "Gravity: ${picture.gravity} m/s^2",
+                ),
+                Text(
+                  picture.semimajorAxis == 0
+                      ? "Semimajor Axis: Unknown"
+                      : "Semimajor Axis: ${picture.semimajorAxis} km",
+                ),
                 Text("Orbital inclination: ${picture.inclination}º"),
                 Text(
-                    "Mass: ${picture.massValue} · 10^${picture.massExponent} kg"),
+                  picture.massValue == 0
+                      ? "Mass: Unknown"
+                      : "Mass: ${picture.massValue} · 10^${picture.massExponent} kg",
+                ),
                 Text(
-                    "Volume: ${picture.volValue} · 10^${picture.volExponent} km^3"),
-                Text("Density: ${picture.density} g/cm^3"),
-                Text("Mean Radius: ${picture.meanRadius}"),
-                Text("Sideral Orbit Period: ${picture.sideralOrbit} days"),
-                Text("Sideral Rotation: ${picture.sideralRotation} hours"),
+                  picture.volValue == 0
+                      ? "Volume: Unknown"
+                      : "Volume: ${picture.volValue} · 10^${picture.volExponent} km^3",
+                ),
+                Text(
+                  picture.density == 0
+                      ? "Density: Unknown"
+                      : "Density: ${picture.density} g/cm^3",
+                ),
+                Text(
+                  picture.meanRadius == 0
+                      ? "Mean radius: Unknown"
+                      : "Mean Radius: ${picture.meanRadius}",
+                ),
+                Text(
+                  picture.sideralOrbit == 0
+                      ? "Sideral Orbit Period: Unknown"
+                      : "Sideral Orbit Period: ${picture.sideralOrbit} days",
+                ),
+                Text(
+                  picture.sideralRotation == 0
+                      ? "Sideral Rotation: Unknown"
+                      : "Sideral Rotation: ${picture.sideralRotation} hours",
+                ),
                 Text("Axialt Tilt: ${picture.axialTilt}º"),
                 Text("Mean Temperature: ${picture.avgTemp} K"),
-                Text("Discovered by: ${picture.discoveredBy}"),
-                Text("Discovery date: ${picture.discoveryDate}"),
-                Text("Alternative name: ${picture.alternativeName}"),
+                Text(
+                  picture.discoveredBy == ""
+                      ? "Discovered by: Unknown"
+                      : "Discovered by: ${picture.discoveredBy}",
+                ),
+                Text(
+                  picture.discoveryDate == ""
+                      ? "Discovery date: Unknown"
+                      : "Discovery date: ${picture.discoveryDate}",
+                ),
+                Text(
+                  picture.alternativeName == ""
+                      ? "Alternative name: -"
+                      : "Alternative name: ${picture.alternativeName}",
+                ),
               ],
             ),
           );
