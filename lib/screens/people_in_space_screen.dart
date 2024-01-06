1d9f0cd0b1ae6f52736e8b303ae93b8c7dc563c9
AriSevcik <99729800+AriSevcik@users.noreply.github.com>
2024-01-06 12:20:33 +0100

    people in space screen done

--- lib/screens/people_in_space_screen.dart
+++ lib/screens/people_in_space_screen.dart
@@ -12,8 +12,8 @@ class PeopleInSpaceScreen extends StatelessWidget {
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(
-              child: CircularProgressIndicator(),
-            );
+            child: CircularProgressIndicator(),
+          );
         } else if (snapshot.hasError) {
           return Text('Error: ${snapshot.error}');
         } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
@@ -36,22 +36,36 @@ class PeopleInSpaceScreen extends StatelessWidget {
               child: Column(
                 children: [
                   Align(
-                  alignment: Alignment.topLeft,
-                  child: BackButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
+                    alignment: Alignment.topLeft,
+                    child: BackButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
                   ),
-                ),
-                const Spacer(),
-                const Text("People currently on space:", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-                const Spacer(),
-                Text((peopleNum.toString()), style: const TextStyle(color: Color.fromARGB(255, 219, 230, 240), fontSize: 120, fontWeight: FontWeight.bold)),
-                const Spacer(),
-                //SpacePeopleList(peopleBySpacecraft: peopleBySpacecraft),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  const Text("People currently on space:",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Text((peopleNum.toString()),
+                      style: const TextStyle(
+                          color: Color.fromARGB(255, 219, 230, 240),
+                          fontSize: 120,
+                          fontWeight: FontWeight.bold)),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Expanded(
+                      child: SpacePeopleList(
+                          peopleBySpacecraft: peopleBySpacecraft)),
                 ],
-                ),
               ),
+            ),
           );
         }
       },
@@ -70,42 +84,44 @@ class SpacePeopleList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-                itemCount: peopleBySpacecraft.length,
-                itemBuilder: (context, index) {
-                  String spacecraft = peopleBySpacecraft.keys.elementAt(index);
-                  List<PeopleInSpace> peopleOnSpacecraft =
-    peopleBySpacecraft[spacecraft]!;
-    
-                  return Card(
-                    margin: const EdgeInsets.all(8.0),
-                    child: Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(
-          'Spacecraft: $spacecraft',
-          style: const TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 18.0,
-          ),
-        ),
-      ),
-      Column(
-        children: peopleOnSpacecraft
-            .map(
-              (person) => ListTile(
-                title: Text(person.name),
+      itemCount: peopleBySpacecraft.length,
+      itemBuilder: (context, index) {
+        String spacecraft = peopleBySpacecraft.keys.elementAt(index);
+        List<PeopleInSpace> peopleOnSpacecraft =
+            peopleBySpacecraft[spacecraft]!;
+
+        return Card(
+          margin: const EdgeInsets.all(4.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Spacecraft: $spacecraft',
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
+                      color: Color.fromARGB(255, 30, 20, 44)),
+                ),
               ),
-            )
-            .toList(),
-      ),
-    ],
-                    ),
-                  );
-                },
-              );
+              Column(
+                children: peopleOnSpacecraft
+                    .map(
+                      (person) => ListTile(
+                        title: Text(
+                          person.name,
+                          style: const TextStyle(
+                              color: Color.fromARGB(255, 30, 20, 44)),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
-
-//SpacePeopleList
