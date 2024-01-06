bb20338bc400eab6498ee4fc5fdac38f4f467b01
AriSevcik <99729800+AriSevcik@users.noreply.github.com>
2024-01-06 03:10:36 +0100

    people in space shows only number of people

--- lib/screens/people_in_space_screen.dart
+++ lib/screens/people_in_space_screen.dart
@@ -6,11 +6,14 @@ class PeopleInSpaceScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int peopleNum = 0;
     return FutureBuilder<List<PeopleInSpace>>(
       future: loadPeopleInSpace(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return const CircularProgressIndicator();
+          return const Center(
+              child: CircularProgressIndicator(),
+            );
         } else if (snapshot.hasError) {
           return Text('Error: ${snapshot.error}');
         } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
@@ -24,56 +27,85 @@ class PeopleInSpaceScreen extends StatelessWidget {
               peopleBySpacecraft[person.craft] = [];
             }
             peopleBySpacecraft[person.craft]!.add(person);
+            peopleNum++;
           }
 
           // Display data
-          return ListView.builder(
-            itemCount: peopleBySpacecraft.length,
-            itemBuilder: (context, index) {
-              String spacecraft = peopleBySpacecraft.keys.elementAt(index);
-              List<PeopleInSpace> peopleOnSpacecraft =
-                  peopleBySpacecraft[spacecraft]!;
-
-              return Card(
-                margin: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: const Text('Back'),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'Spacecraft: $spacecraft',
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      children: peopleOnSpacecraft
-                          .map(
-                            (person) => ListTile(
-                              title: Text(person.name),
-                            ),
-                          )
-                          .toList(),
-                    ),
-                  ],
+          return Scaffold(
+            body: Center(
+              child: Column(
+                children: [
+                  Align(
+                  alignment: Alignment.topLeft,
+                  child: BackButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
                 ),
-              );
-            },
+                const Spacer(),
+                const Text("People currently on space:", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+                const Spacer(),
+                Text((peopleNum.toString()), style: const TextStyle(color: Color.fromARGB(255, 219, 230, 240), fontSize: 120, fontWeight: FontWeight.bold)),
+                const Spacer(),
+                //SpacePeopleList(peopleBySpacecraft: peopleBySpacecraft),
+                ],
+                ),
+              ),
           );
         }
       },
     );
   }
 }
+
+class SpacePeopleList extends StatelessWidget {
+  const SpacePeopleList({
+    super.key,
+    required this.peopleBySpacecraft,
+  });
+
+  final Map<String, List<PeopleInSpace>> peopleBySpacecraft;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+                itemCount: peopleBySpacecraft.length,
+                itemBuilder: (context, index) {
+                  String spacecraft = peopleBySpacecraft.keys.elementAt(index);
+                  List<PeopleInSpace> peopleOnSpacecraft =
+    peopleBySpacecraft[spacecraft]!;
+    
+                  return Card(
+                    margin: const EdgeInsets.all(8.0),
+                    child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          'Spacecraft: $spacecraft',
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18.0,
+          ),
+        ),
+      ),
+      Column(
+        children: peopleOnSpacecraft
+            .map(
+              (person) => ListTile(
+                title: Text(person.name),
+              ),
+            )
+            .toList(),
+      ),
+    ],
+                    ),
+                  );
+                },
+              );
+  }
+}
+
+//SpacePeopleList
